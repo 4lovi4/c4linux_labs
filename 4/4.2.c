4f03bdc35026564f5856bb9b899ea681696e7376
alexey <alexey.dyatlov@yandex.ru>
2016-11-18 00:02:53 +0300

    add select()

--- 4/4.2.c
+++ 4/4.2.c
@@ -20,16 +20,25 @@ int read_num(long int *num, int fd)
 }
 int main (int argc, char **argv)
 {
-        int fd1, fd2;
+        int fd1, fd2, nfds;
         long int num1, num2, sum;
+        fd_set fds;
 
-        fd1 = open("./in1", O_RDONLY);
-        fd2 = open("./in2", O_RDONLY);
+        fd1 = open("./in1", O_RDONLY|O_NONBLOCK);
+        fd2 = open("./in2", O_RDONLY|O_NONBLOCK);
         if (fd1 < 0 || fd2 < 0) {
                 perror("open()");
                 exit(errno);
         }
-        
+        for(;;) {
+                FD_ZERO(&fds);
+                FD_SET(fd1, &fds);
+                FD_SET(fd2, &fds);
+
+                nfds = (fd1 > fd2 ? fd1 : fd2) + 1;
+
+                select (nfds, &fds, NULL, NULL, NULL);
+        } 
         close(fd1);
         close(fd2);
         return 0;
