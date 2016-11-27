8ca03ad66e47a7da7871185e99740f32f540ef99
alexey <alexey.dyatlov@yandex.ru>
2016-11-27 22:57:23 +0300

    furthe changes

--- 4/4.4.c
+++ 4/4.4.c
@@ -5,11 +5,20 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <signal.h>
+ 
+static sig_atomic_t sig_status = 0;
+
+void usr1_hand()
+{
+        sig_status = 1;
+}
 
 int main(int argc, char **argv)
 {
         pid_t p, s;
         int i;
+        struct sigaction usr1_act;
         p = fork();
         if (p == -1) {
                perror("fork()");
@@ -25,11 +34,23 @@ int main(int argc, char **argv)
                         perror("setsid()");
                         exit(errno);
                 }
-                printf("getpid() = %ld;\n\
-setsid() = %ld;\n", (long int)getpid(), (long int)s);
+                printf("%ld\n", (long int)s);
+//setsid() = %ld;\n", (long int)getpid(), (long int)s);
                 for (i = 0; i < 3; i++) 
                         close(i);
-                sleep(60);
+                sigemptyset(&usr1_act.sa_mask);
+                usr1_act.sa_flags = 0;
+                usr1_act.sa_handler = (void *)&usr1_hand;
+
+                for(;;) {
+                        if (sigaction(SIGURG, &usr1_act, NULL) < 0) {
+                                perror("sigaction(SIGUSR1)");
+                                return 1;
+                        }
+                if (sig_status == 1)
+                        break;
+                }
+                _exit(0);
         }
-                return 0;
+        return 0;
 }
---
+++ 4/4.5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main (int argc, char **argv) 
+{
+
+        return 0;
+}
+
