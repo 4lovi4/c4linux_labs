a4830ec366ead86bddc96e4e42c21a8637ede6a3
alexey <alexey.dyatlov@yandex.ru>
2016-11-27 13:35:31 +0300

    futher commit

--- 4/4.2.c
+++ 4/4.2.c
@@ -10,18 +10,22 @@
 int read_num(long int *num, int fd) 
 {
         char buf[512];
+        int rb;
         memset(buf, 0, sizeof(buf));
-        if (read(fd, buf, sizeof(buf)) < 0) {
+        rb = read(fd, buf, sizeof(buf));
+        if(rb < 0) {
                 perror("read()");
-                exit(errno);
+                return(rb);
         }
-        *num = strtol(buf, NULL, 10);
-        return 0;
+        else if (rb > 0)
+                *num = strtol(buf, NULL, 10);
+        return rb;
 }
 int main (int argc, char **argv)
 {
-        int fd1, fd2, nfds;
-        long int num1, num2, sum;
+        int fd1, fd2, nfds,
+            r1, r2;
+        long int num, sum = 0;
         fd_set fds;
 
         fd1 = open("./in1", O_RDONLY|O_NONBLOCK);
@@ -29,7 +33,8 @@ int main (int argc, char **argv)
         if (fd1 < 0 || fd2 < 0) {
                 perror("open()");
                 exit(errno);
-        }
+                }
+
         for(;;) {
                 FD_ZERO(&fds);
                 FD_SET(fd1, &fds);
@@ -38,7 +43,21 @@ int main (int argc, char **argv)
                 nfds = (fd1 > fd2 ? fd1 : fd2) + 1;
 
                 select (nfds, &fds, NULL, NULL, NULL);
+
+                if (FD_ISSET(fd1, &fds)) {
+                        r1 = read_num(&num, fd1);
+                }
+                if (FD_ISSET(fd2, &fds)) {
+                        r2 = read_num(&num, fd2);
+                }
+
+                if ((r1 || r2) == 0)
+                        break;
+
+                fprintf(stderr,"num = %ld;\n",num);
+                sum += num;
         } 
+        printf("%ld\n", sum);
         close(fd1);
         close(fd2);
         return 0;
--- 4/solution.c
+++ 4/solution.c
@@ -1,41 +1,61 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
 #include <errno.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+
+static int usr_count1 = 0, 
+           usr_count2 = 0;
+static sig_atomic_t sig_status = 0;
+
+void usr1_handler(void)
+{
+        usr_count1++;
+}
+
+void usr2_handler(void)
+{
+        usr_count2++;
+}
+
+void term_handler(void)
+{
+        sig_status = 1;
+}
 
 int main (int argc, char **argv)
 {
-        FILE *f;
-        const char *read_flag = "r";
-        char c;
-        long unsigned int n = 0;
-        char pstring[80];
-       
-        if (argc < 3) {
-                fprintf(stderr, "Two few arguments, should be two of them!\n\
-\t%s <command> <parameter>\n", argv[0]);
-                exit(1); 
-        }
-        memset(pstring, 0, sizeof(pstring));
-        sprintf(pstring, "%s %s", argv[1], argv[2]);
-       
-        f = popen(pstring, read_flag);
+        struct sigaction usr_act1, usr_act2, term_act;
+        
+        sigemptyset(&usr_act1.sa_mask);
+        sigemptyset(&usr_act2.sa_mask);
+        sigemptyset(&term_act.sa_mask);
+        usr_act1.sa_flags = 0;
+        usr_act2.sa_flags = 0;
+        term_act.sa_flags = 0;
+        usr_act1.sa_handler = (void *)&usr1_handler;
+        usr_act2.sa_handler = (void *)&usr2_handler;
+        term_act.sa_handler = (void *)&term_handler;
 
-        if (f == NULL) {
-                perror("popen()");
-                if (errno) 
-                        exit(errno);
-                else
-                        exit(1);
+        for(;;) {
+                if (sigaction(SIGUSR1, &usr_act1, NULL) < 0) {
+                        perror("sigaction(SIGUSR1)");
+                        return 1;
+                }
+
+                if (sigaction(SIGUSR2, &usr_act2, NULL) < 0) {
+                        perror("sigaction(SIGUSR2)");
+                        return 1;
+                }
+
+                if (sigaction(SIGTERM, &term_act, NULL) < 0) {
+                        perror("sigaction(SIGUSR1)");
+                        return 1;
+                }
+                if (sig_status == 1)
+                        break;
         }
-        while (c != EOF) {
-                c = (char)fgetc(f);
-                if (c == '0')
-                        n++;
-        }       
-        printf("%ld\n", n);
-        pclose(f);
+
+        printf("%d %d\n", usr_count1, usr_count2);
+
         return 0;
-
 }
